16ed6b8ec444d73833dd592d83c267ead675d228
nicolasarruda <nlarruda@hotmail.com>
2022-06-11 18:14:09 -0300

    Menu and Responsiveness

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,13 +1,27 @@
 import './styles.css';
+import 'bootstrap/js/src/collapse.js';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md bg-primary main-nav">
+    <nav className="navbar navbar-expand-sm navbar-light bg-primary main-nav">
       <div className="container-fluid">
         <a href="link" className="nav-logo-text">
           <h4>Carros Top</h4>
         </a>
-        <div className="collapse navbar-collapse">
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#car-catalog-navbar"
+          aria-controls="car-catalog-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler"></span>
+        </button>
+
+        <div className="collapse navbar-collapse offset-md-2" id="car-catalog-navbar">
           <ul className="navbar-nav main-menu">
             <li>
               <a href="link" className="active">Home</a>
